11034a8cbff417ed11bf67318aa8312619070969
Matt Schreiber <schreibah@gmail.com>
2023-03-16 09:44:43 -0400

    feat(tests): put all generated assets in tempdir
    rather than directly in `/tmp` (`/tmp/clipdir`) or in singleton
    temporary files (e.g. `log=$(mktemp)`, previously in `tests/temp-perf`).
    
    This change:
    
        1. Enables a simplified and unified interface for cleaning up
           generated assets via the newly-introduced `cleanup` EXIT hook
           function, plus the user-specifiable `cleanup_pre` function, and
        2. Prevents clobbering the user's existing clipboard cache.
    
    Incidental change: remove the definition of `clipmenu_shim` from
    `tests/clipmenu-perf`; this variable is unused.

--- tests/setup.sh
+++ tests/setup.sh
@@ -6,9 +6,27 @@ toplevel() {
     pwd -P
 }
 
+cleanup() {
+    if command -v cleanup_pre &>/dev/null; then
+        cleanup_pre
+    fi
+
+    if [[ -n "${tempdir:-}" ]] && [[ -d "$tempdir" ]]; then
+        # Try to use coreutils-specific args for safety; fall back to
+        # busybox-compatible args if necessary.
+        rm --one-file-system --preserve-root -rf "$tempdir" || rm -rf "$tempdir"
+    fi
+}
+
 toplevel="$(toplevel)"
 
 export PATH="${toplevel}${PATH:+:${PATH}}"
+
+tempdir="$(mktemp -d)"
+
+trap cleanup EXIT
+
+export CM_DIR="$tempdir"
 
 dir=$(clipctl cache-dir)
 
